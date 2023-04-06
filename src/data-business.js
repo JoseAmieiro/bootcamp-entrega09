325b7a9361c6ea5e6b4d9f60f898f5fbbcbc79fb
JoseAmieiro <joseperezamieiro@gmail.com>
2023-04-06 13:19:54 +0200

    Ejercicio extra sin acabar

---
+++ src/data-business.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+function getCharacters() {
+  return axios
+    .get("https://rickandmortyapi.com/api/character")
+    .then((response) => {
+      return response.data;
+    });
+}
+function getCharacterById(id) {
+  return axios
+    .get("https://rickandmortyapi.com/api/character/" + id)
+    .then((response) => {
+      return response.data;
+    });
+}
+export { getCharacters, getCharacterById };
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import * as DataBusiness from "./data-business";
+import * as Utils from "./utils";
+
+DataBusiness.getCharacters().then((data) => {
+  document.getElementById("root").innerText = "";
+  const users = data.results;
+
+  const nodes = [];
+  for (let user of users) {
+    const node = Utils.createCharacterRow(user);
+    node.onclick = function () {
+      DataBusiness.getCharacterById(user.char_id).then((dataBis) => {
+        Utils.showCharacter(dataBis);
+      });
+    };
+
+    nodes.push(node);
+  }
+
+  for (let node of nodes) {
+    document.getElementById("root").append(node);
+  }
+});
